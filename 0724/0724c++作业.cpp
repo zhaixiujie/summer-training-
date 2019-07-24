d238eb139384adc73f2eefdf35afd4143dde53d6
zhaixiujie <xxsfno@126.com>
2019-07-24 15:04:04 +0800

    上传c++作业

---
+++ 0724/0724c++作业.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+using namespace std;
+struct SummerTrain
+{
+	char number[20];
+	int age;
+	string name;
+	string address;
+};
+int main()
+{
+	SummerTrain xiu =
+	{
+		"P19301161",
+		18,
+		"–„–„",
+		"ª›‘∞6137",
+
+	};
+	cout << "—ß∫≈£∫" << xiu.number << endl;
+	cout << "ƒÍ¡‰£∫" << xiu.age << endl;
+	cout << "–’√˚£∫" << xiu.name << endl;
+	cout << "µÿ÷∑£∫" << xiu.address << endl;
+
+
+	system("pause");
+	return 0;
+}
